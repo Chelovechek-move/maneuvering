d7bf2bbab4119023a9dcf9a191a20354ec906af4
vladimir chiniaev <chiniaev.vn@mipt.ru>
2025-09-22 10:22:24 +0300

    Добавил функции в init

---
+++ src/maneuvering/maneuvers/__init__.py
@@ -0,0 +1,3 @@
+from .quasi_circular.transition.execute import execute
+
+__all__ = ["execute"]
--- src/maneuvering/orbit/__init__.py
+++ src/maneuvering/orbit/__init__.py
@@ -1,6 +1,10 @@
 from .cartesian import Cart
-from .convert import convert_cart_to_kep_true, convert_kep_true_to_cart
+from .convert_kep_cart import convert_cart_to_kep_true, convert_kep_true_to_cart
+from .convert_kep import calc_mean_from_true, calc_true_from_eccentric, calc_eccentric_from_mean, calc_true_from_mean, \
+    calc_true_from_mean_non_norm, calc_eccentric_from_true, calc_mean_from_eccentric
 from .keplerian import Kep, KepEcc, KepMean, KepTrue, kep_ecc, kep_mean, kep_true, keplerian
+from .propagate import propagate_mean, propagate_mean_anomaly, propagate_true_anomaly, propagate_true
+from .distance import distance_orbit
 
 __all__ = [
     "Kep",
@@ -14,4 +18,16 @@ __all__ = [
     "Cart",
     "convert_kep_true_to_cart",
     "convert_cart_to_kep_true",
+    "calc_mean_from_eccentric",
+    "calc_eccentric_from_true",
+    "calc_mean_from_true",
+    "calc_true_from_eccentric",
+    "calc_eccentric_from_mean",
+    "calc_true_from_mean",
+    "calc_true_from_mean_non_norm",
+    "propagate_mean",
+    "propagate_true",
+    "propagate_true_anomaly",
+    "propagate_mean_anomaly",
+    "distance_orbit"
 ]
