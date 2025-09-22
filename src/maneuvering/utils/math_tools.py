4e6f171c58c4d556faf43979a55e3a482f60fafd
vladimir chiniaev <chiniaev.vn@mipt.ru>
2025-09-22 10:24:47 +0300

    Написал функцию для нормализации углов

--- src/maneuvering/utils/math_tools.py
+++ src/maneuvering/utils/math_tools.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from maneuvering.types import Vector3
+from maneuvering.types import Scalar, Vector3
 
 
 def normalize(v: Vector3) -> Vector3:
@@ -19,3 +19,22 @@ def normalize(v: Vector3) -> Vector3:
     """
     n = float(np.linalg.norm(v))
     return v / n
+
+
+def normalize_angle(a: Scalar) -> Scalar:
+    """
+    Нормализует угол в диапазон [0, 2π).
+
+    Параметры
+    ----------
+    a : Scalar
+        Угол, [рад].
+
+    Возвращает
+    -------
+    Scalar
+        Нормализованный угол в диапазоне [0, 2π), [рад].
+    """
+    twopi = 2.0 * np.pi
+    x = float(a) % twopi
+    return x + twopi if x < 0.0 else x
